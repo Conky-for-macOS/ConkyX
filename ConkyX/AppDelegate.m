c7c34b7c188cf91000688076fb29f4776e9ec145
npyl <n.pylarinos@hotmail.com>
2018-12-20 02:40:27 +0200

    Update AppDelegate.m
    This should improve the experience: ConkyX should close when close button is pressed.

--- ConkyX/AppDelegate.m
+++ ConkyX/AppDelegate.m
@@ -8,20 +8,11 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
 }
-
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
-
 
 @end
